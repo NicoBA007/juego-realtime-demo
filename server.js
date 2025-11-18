62aed58d54fb4af2dead45cfed6825f470af418e
NicoBA007 <barrancosnicolas64@gmail..com>
2025-11-17 21:39:41 -0400

    Fix: Bindea el servidor a 0.0.0.0 para Render

--- server.js
+++ server.js
@@ -70,7 +70,10 @@ io.on('connection', (socket) => {
 });
 
 // 6. Iniciar el servidor
+// ASÍ DEBE ESTAR (CORRECTO)
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+const HOST = '0.0.0.0'; // Escuchar en todas las interfaces
+
+server.listen(PORT, HOST, () => {
+  console.log(`¡Servidor escuchando en el puerto ${PORT}!`);
 });
